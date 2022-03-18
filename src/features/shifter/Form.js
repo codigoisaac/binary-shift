07a6cfa2272d6459d1387fdc50ef474a9ea14c2f
Isaac Muniz <isaacmuniz@protonmail.com>
2022-03-17 23:05:49 -0300

    Copy button

--- src/features/shifter/Form.js
+++ src/features/shifter/Form.js
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { TextField } from './TextField';
 import { BinaryField } from './BinaryField';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { textInserted, initialState } from './shifterSlice';
 
 export const Form = () => {
   const dispatch = useDispatch();
+  const shift = useSelector((state) => state.shifter);
 
   useEffect(() => {
     dispatch(textInserted(initialState.text));
@@ -15,6 +16,15 @@ export const Form = () => {
     <div>
       <TextField />
       <BinaryField />
+
+      <div>
+        <button
+          className="text-black font-bold border border-neutral-200 p-2 rounded bg-neutral-600"
+          onClick={() => navigator.clipboard.writeText(shift.binary)}
+        >
+          Copy <small className="font-semibold">(Ctrl + B)</small>
+        </button>
+      </div>
     </div>
   );
 };
