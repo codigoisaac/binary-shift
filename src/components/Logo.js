e484425eab03df00d7382cfd88031b7474676855
Isaac Muniz <isaacmuniz@protonmail.com>
2022-03-28 18:25:44 -0300

    Add my link

--- src/components/Logo.js
+++ src/components/Logo.js
@@ -2,6 +2,8 @@ import React, { useState } from 'react';
 
 export const Logo = () => {
   const initialTitle = 'Binary Shift',
+    initialBinary =
+      '01000010 01101001 01101110 01100001 01110010 01111001 00100000 01010011 01101000 01101001 01100110 01110100',
     initialClass = 'text-4xl md:text-6xl';
 
   const [display, setDisplay] = useState(initialTitle);
@@ -12,9 +14,7 @@ export const Logo = () => {
       setDisplay(initialTitle);
       setClass(initialClass);
     } else {
-      setDisplay(
-        '01000010 01101001 01101110 01100001 01110010 01111001 00100000 01010011 01101000 01101001 01100110 01110100'
-      );
+      setDisplay(initialBinary);
       setClass('text-lg');
     }
   };
@@ -31,7 +31,14 @@ export const Logo = () => {
         </div>
         <div>
           by{' '}
-          <span className="hover:text-[#00f] cursor-pointer">codigoisaac</span>
+          <a
+            href="https://campsite.bio/codigoisaac"
+            target="_blank"
+            rel="noreferrer"
+            className="hover:text-[#00f] cursor-pointer"
+          >
+            codigoisaac
+          </a>
         </div>
       </div>
     </div>
