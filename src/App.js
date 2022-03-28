83228ca55dce6b78ef6c67fc0e2926e0e888e143
Isaac Muniz <isaacmuniz@protonmail.com>
2022-03-28 15:26:14 -0300

    Add logo

--- src/App.js
+++ src/App.js
@@ -4,9 +4,16 @@ import './index.css';
 
 function App() {
   return (
-    <div className="flex justify-center min-h-screen items-center bg-neutral-900">
-      <div className="w-screen md:w-9/12 lg:w-5/12">
-        <Form />
+    <div className="bg-neutral-900 min-h-screen">
+      <div className="text-neutral-500 font-['Space_Grotesk'] p-3 w-fit mx-auto mb-12">
+        <div className="text-3xl md:text-6xl font-bold">Binary Shift</div>
+        <div>by codigoisaac</div>
+      </div>
+
+      <div className="flex justify-center items-center px-2">
+        <div className="w-screen md:w-9/12 lg:w-5/12">
+          <Form />
+        </div>
       </div>
     </div>
   );
